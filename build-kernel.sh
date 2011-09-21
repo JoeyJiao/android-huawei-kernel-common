87e1d8c22271c94a2db4e7f2a213233d0f0d423c
Joey Jiao <joey.jiaojg@gmail.com>
2011-09-21 21:27:03 +0800

    First release 2.6.32.9 kernel for c8600,c8500,m860 with multi touch support and many other functions.

---
+++ build-kernel.sh
@@ -0,0 +1,2 @@
+#ARCH=arm CROSS_COMPILE=~/android/src/c8600/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- make V=1 zImage -j2
+ARCH=arm CROSS_COMPILE=~/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- make zImage -j2
---
+++ build-modules.sh
@@ -0,0 +1,1 @@
+ARCH=arm CROSS_COMPILE=~/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- make modules -j2
